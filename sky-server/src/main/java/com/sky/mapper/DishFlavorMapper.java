b875e652105579822dfbb23592e5e327d43e589e
Theodore <15801559922@163.com>
2023-09-15 20:55:34 +0800

    修改菜品

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -10,12 +11,14 @@ import java.util.List;
 public interface DishFlavorMapper {
     /**
      * 批量插入口味
+     *
      * @param flavors
      */
     void insertBatch(List<DishFlavor> flavors);
 
     /**
      * 根据菜品id删除对应的口味数据
+     *
      * @param dishId
      */
     @Delete("delete from dish_flavor where dish_id = #{dishId}")
@@ -23,7 +26,17 @@ public interface DishFlavorMapper {
 
     /**
      * 根据菜品id集合批量删除关联的口味数据
+     *
      * @param dishIds
      */
     void deleteByDishIds(List<Long> dishIds);
+
+    /**
+     * 根据菜品id查询对应的口味数据
+     *
+     * @param dishId
+     * @return
+     */
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
 }
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -57,4 +57,12 @@ public interface DishMapper {
      * @param ids
      */
     void deleteByIds(List<Long> ids);
+
+    /**
+     * 根据id动态修改擦品数据
+     * @param dish
+     */
+
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Dish dish);
 }
