2552de0bc8d17e9c119e2c03eb0f709f216db783
Theodore <15801559922@163.com>
2023-10-11 18:17:11 +0800

    接单拒单

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,9 +1,9 @@
 package com.sky.service;
 
-import com.sky.dto.OrdersPaymentDTO;
-import com.sky.dto.OrdersSubmitDTO;
+import com.sky.dto.*;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
 
@@ -33,6 +33,7 @@ public interface OrderService {
 
     /**
      * 用户端订单分页查询
+     *
      * @param page
      * @param pageSize
      * @param status
@@ -42,6 +43,7 @@ public interface OrderService {
 
     /**
      * 查询订单详情
+     *
      * @param id
      * @return
      */
@@ -49,13 +51,41 @@ public interface OrderService {
 
     /**
      * 用户取消订单
+     *
      * @param id
      */
     void userCancelById(Long id) throws Exception;
 
     /**
      * 再来一单
+     *
      * @param id
      */
     void repetition(Long id);
+
+    /**
+     * 条件搜索订单
+     *
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 各个状态的订单数量统计
+     * @return
+     */
+    OrderStatisticsVO statistics();
+
+    /**
+     * 接单
+     * @param ordersConfirmDTO
+     */
+    void confirm(OrdersConfirmDTO ordersConfirmDTO);
+
+    /**
+     * 拒单
+     * @param ordersRejectionDTO
+     */
+    void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
 }
