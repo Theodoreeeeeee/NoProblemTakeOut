74da8669de3f7a73cd01e29a33bdc93a4defc166
Theodore <15801559922@163.com>
2023-10-12 00:20:11 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,16 +6,21 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     /**
      * 插入订单数据
+     *
      * @param orders
      */
     void insert(Orders orders);
 
     /**
      * 根据订单号查询订单
+     *
      * @param orderNumber
      */
     @Select("select * from orders where number = #{orderNumber}")
@@ -23,6 +28,7 @@ public interface OrderMapper {
 
     /**
      * 修改订单信息
+     *
      * @param orders
      */
     void update(Orders orders);
@@ -30,6 +36,7 @@ public interface OrderMapper {
 
     /**
      * 分页条件查询并按下单时间排序
+     *
      * @param ordersPageQueryDTO
      * @return
      */
@@ -37,6 +44,7 @@ public interface OrderMapper {
 
     /**
      * 根据id查询订单
+     *
      * @return
      */
     @Select("select * from orders where id = #{id}")
@@ -44,9 +52,14 @@ public interface OrderMapper {
 
     /**
      * 根据状态统计订单数量
+     *
      * @param status
      * @return
      */
     @Select("select count(id)  from orders where status = #{status}")
     Integer countStatus(Integer status);
+
+
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
