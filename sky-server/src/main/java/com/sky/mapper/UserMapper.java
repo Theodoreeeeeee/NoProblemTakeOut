3d09db2c2929d53cd0e4e755c91625b8c078e218
Theodore <15801559922@163.com>
2023-10-14 19:29:11 +0800

    用户数量统计

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,9 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+import java.util.Objects;
+
 @Mapper
 public interface UserMapper {
 
@@ -31,4 +34,11 @@ public interface UserMapper {
      */
     @Select("select * from user where id = #{userId}")
     User getById(Long userId);
+
+    /**
+     * 根据动态条件统计用户数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map<String, Object> map);
 }
